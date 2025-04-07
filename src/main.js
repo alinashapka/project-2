0993c5fc6b13c5cd3f74bd0bd44574ce81adfe7e
alina <alinkash2811@gmail.com>
2025-04-07 17:11:25 +0200

    initial commit and add faq

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '@fontsource/inter-tight/400.css';
+import '@fontsource/inter-tight/500.css';
+import '@fontsource/inter-tight/600.css';
+import '@fontsource/inter-tight/700.css';
+import '@fontsource/inter-tight/800.css';
+import '@fontsource/inter-tight/900.css';
+// зайві потім приберем
+
+import { AccordionFaq } from "./js/faq.js";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  AccordionFaq();
+});
